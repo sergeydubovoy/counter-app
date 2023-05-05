41fa602af7eaaf902ece2c89a15233e56749b70b
Sergey Dubovoy <sergeydubovoi@gmail.com>
2023-05-05 20:39:06 +0300

    Added JS code fot bonus function

--- index.js
+++ index.js
@@ -3,34 +3,43 @@ const buttonPlusOne = document.querySelector('[data-js="btn-plus-one"]');
 const buttonMinusOne = document.querySelector('[data-js="btn-minus-one"]');
 const buttonMinusTen = document.querySelector('[data-js="btn-minus-ten"]');
 const buttonReset = document.querySelector('[data-js="btn-reset"]');
-const counterText = document.querySelector(".counter");
+const counterText = document.querySelector('[data-js="counter"]');
+const buttonBonus = document.querySelector('[data-js="btn-bonus"]');
 
 const COUNTER_INITIAL_VALUE = 0;
 let counter = COUNTER_INITIAL_VALUE;
 
 counterText.innerText = counter;
 
-buttonPlusTen.addEventListener("click", function () {
-  counter = counter + 10;
+function updateCounter(value) {
+  counter += value;
   counterText.innerText = counter;
+
+  if (counter === 100) {
+    buttonReset.classList.add("btn-reset-small");
+    buttonBonus.classList.add("bonus-active");
+  }
+}
+
+buttonPlusTen.addEventListener("click", function () {
+  updateCounter(10);
 });
 
 buttonPlusOne.addEventListener("click", function () {
-  counter = counter + 1;
-  counterText.innerText = counter;
+  updateCounter(1);
 });
 
 buttonMinusOne.addEventListener("click", function () {
-  counter = counter - 1;
-  counterText.innerText = counter;
+  updateCounter(-1);
 });
 
 buttonMinusTen.addEventListener("click", function () {
-  counter = counter - 10;
-  counterText.innerText = counter;
+  updateCounter(-10);
 });
 
 buttonReset.addEventListener("click", function () {
   counter = COUNTER_INITIAL_VALUE;
   counterText.innerText = counter;
+  buttonReset.classList.remove("btn-reset-small");
+  buttonBonus.classList.remove("bonus-active");
 });
